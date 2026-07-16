922deae2fbd0d9b050120a96f4af0ae5956121cb
lauandanobre <lauanda.nobre@gmail.com>
2026-07-16 16:19:56 -0300

    Remoção do pacote hera_robot, transferindo para vosk_speech

--- hera_ws/src/hera_robot/utilitarios/showMicrophones.py
+++
@@ -1,7 +0,0 @@
-import pyaudio
-
-p = pyaudio.PyAudio()
-for i in range(p.get_device_count()):
-    dev = p.get_device_info_by_index(i)
-    if dev['maxInputChannels'] > 0:
-        print(f"ID: {i} | Nome: {dev['name']} | Canais: {dev['maxInputChannels']}")
